ab591522929d5a6721c8e74cf038e334d12cb15d
RNJ280 <thibault.leemans@extern.fluvius.be>
2020-10-14 00:04:23 +0200

    Added styling for button

--- src/components/controls/CustomButton.js
+++ src/components/controls/CustomButton.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: theme.spacing(0.5)
+    },
+    label: {
+        textTransform: 'none'
+    }
+}))
 
 function CustomButton(props) {
 
     const {text, size, color, variant, onClick, ...other} = props
+    const classes = useStyles();
+
 
     return (
         <Button
@@ -12,6 +24,7 @@ function CustomButton(props) {
             color={color || "primary"}
             onClick={onClick}
             {...other}
+            classes={{root:classes.root, label:classes.label}}
         >
             {text}
         </Button>
