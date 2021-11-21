c5a75f73add5626a482f03fcb3e114aaaf2af4a8
Phillip <p_w999@ymail.com>
2021-11-21 15:03:25 +0100

    Small improvements to template admin api

--- functions/public/manage_template.js
+++ functions/public/manage_template.js
@@ -1,3 +1,8 @@
+// =================================================================================
+// Template management
+// This class is responsible for inserting a template into the Firebase store.
+// See mail_templates/README.md for more info.
+// =================================================================================
 const tools = require('../tools');
 const functions = require('firebase-functions');
 const {admin, db} = require('../db');
@@ -16,18 +21,36 @@ exports.updateTemplate = functions
         });
       }
 
+      if (req.query.id == null || req.query.id == '') {
+        return res.status(400).send({
+          message: 'The reuest parameter id is mandatory',
+        });
+      }
+
+      if (req.body.html == null || req.body.html == '') {
+        return res.status(400).send({
+          message: 'The body should contain an html body',
+        });
+      }
+
+      if (req.body.subject == null || req.body.subject == '') {
+        return res.status(400).send({
+          message: 'The body should contain a subject',
+        });
+      }
+
       const docId = req.query.id;
       const writeResult = await db
           .collection('mail_templates')
           .doc(docId)
           .set({
-            subject: 'Bevestiging wachtlijst Loppem Conversa',
-            html: req.rawBody.toString(),
+            subject: req.body.subject,
+            html: req.body.html,
           });
       console.info(`Added template with id ${writeResult.id} and body ${req.rawBody.toString()}`);
 
       return cors(req, res, () => {
-        res.status(201).send({
+        res.status(200).send({
           id: docId,
         });
       });
