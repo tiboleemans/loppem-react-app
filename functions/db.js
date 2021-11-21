63c54de83f1b827a1068ab1386cd73be02c52d85
Phillip <p_w999@ymail.com>
2021-11-21 15:03:01 +0100

    Add projectId for tests
    Without this we can't properly use the FireDB api in mocha tests

--- functions/db.js
+++ functions/db.js
@@ -1,6 +1,8 @@
 // https://bigcodenerd.org/split-cloud-functions-firebase/
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp({
+  projectId: 'loppem-adf69',
+});
 const db = admin.firestore();
 
 module.exports = {
