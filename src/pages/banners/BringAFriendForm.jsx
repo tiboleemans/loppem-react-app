7a737f85febfb384c78cd70e1817fea8db5849c4
tleemans <tleemans@dxc.com>
2024-01-06 11:23:21 +0100

    Added bring friend functionality

--- src/pages/banners/BringAFriendForm.jsx
+++ src/pages/banners/BringAFriendForm.jsx
@@ -48,6 +48,7 @@ export default function BringAFriendForm() {
   };
 
   const handleSend = () => {
+    updateErrors();
     setShowValidation(true);
     if (canSend()) {
       setIsLoading(true);
